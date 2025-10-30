d3ab905cf88d3b0c7edf4e2a5e018a99da899538
milodemeester <milodm599@gmail.com>
2025-10-30 16:17:06 +0100

    TEST

--- src/Game.cpp
+++ src/Game.cpp
@@ -8,12 +8,9 @@ void Game::run(){
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
